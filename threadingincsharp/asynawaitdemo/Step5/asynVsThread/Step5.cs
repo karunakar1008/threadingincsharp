9b5f91d6b1ba4488dcc9dc25cd7c35d81177633f
karunakar1008 <karunakar1008@gmail.com>
2020-07-11 20:02:15 +0530

    Added thread examples

--- threadingincsharp/asynawaitdemo/Step5/asynVsThread/Step5.cs
+++ threadingincsharp/asynawaitdemo/Step5/asynVsThread/Step5.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace asynawaitdemo.Step5.asynVsThread
 {
-    class Step4
+    class Step5
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Take away: async await, does not create new threads...");
             Console.WriteLine("Main Thread started.....Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
-            SomeMethod();
+            Thread workerThread = new Thread(SomeMethod);
+            workerThread.Name = "MyThread";
+            workerThread.Start(); //new thread started
             Console.WriteLine("Main Thread code end, does not wait");
             Console.ReadKey();
         }
         static async void SomeMethod()
         {
             Console.WriteLine("Asyn code  running... 5 secsThread Id: {0}", Thread.CurrentThread.ManagedThreadId);
-            await Task.Delay(5000); // This code runs on the main thred
-            Console.WriteLine("Asyn code finished...Thread Id: {0}",Thread.CurrentThread.ManagedThreadId ); //Becuase syncrhronisation context is not present in console app this code will run on a different thread, But in windows forms, wpf it will not work. It is just a call back thread
+            await Task.Delay(5000); //sleep the thread which running this method for 5 secs
+            Console.WriteLine("Asyn code finished...");
         }
     }
 }
