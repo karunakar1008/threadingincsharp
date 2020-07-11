528f5b7ef0e6d6b0670f183bb4c7c2b99a23d36a
karunakar1008 <karunakar1008@gmail.com>
2020-07-11 10:40:07 +0530

    Long running method call synchronously

---
+++ threadingincsharp/asynawaitdemo/Step2/NotepadProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace asynawaitdemo
+{
+  public  class NotepadProcessor
+    {
+        private int countNumberOfChars()
+        {
+            int count = 0;
+
+            using (StreamReader reader=new StreamReader(@"C:\Users\karunakarb\Desktop\TestFile.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How the tread is blocked for a long running method");
+
+            NotepadProcessor notepadProcessor = new NotepadProcessor();
+
+          int textLength=  notepadProcessor.countNumberOfChars();
+
+            Console.WriteLine("Number of chars in the file: {0}",textLength);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ threadingincsharp/asynawaitdemo/Step3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asynawaitdemo
+{
+    class Step3
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("How to call an asyncronous method from a main thread and wait till the results comes from async method");
+            Console.WriteLine("Synchronous programming: Each function will wait tilll the execution of previous functions");
+
+           
+
+            Console.WriteLine("Message at the end");
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
