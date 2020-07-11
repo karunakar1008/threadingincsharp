249d215d2af07eb3534103dc8d6daa10f999766f
karunakar1008 <karunakar1008@gmail.com>
2020-07-11 11:39:43 +0530

    get data from database on a single threaded way wothoit blocking main thread.

---
+++ threadingincsharp/asynawaitdemo/Step4/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asynawaitdemo.Step4
+{
+    
+    class Customer
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Main thread Started");
+
+            GetAllCustomertails();
+            Console.WriteLine("Main thread do not wait for worker thread to be completed");
+
+            Console.ReadKey();
+
+        }
+
+        public static async void GetAllCustomertails()
+        {
+            Console.WriteLine("Fetch Employee Details one by one");
+
+            for (int i = 1; i <= 4; i++)
+            {
+                await RunMetodOnDifferentThread(i);
+            }
+
+            Console.WriteLine("All employee details are fetced successfully!");
+        }
+
+        public static async Task RunMetodOnDifferentThread(int index)
+        {
+
+            Console.WriteLine("New Thread started at: {0}", DateTime.Now);
+
+            //await Task.Run(new Action(()=>GetCustomerDetails(index)));
+
+            await Task.Run(() => GetCustomerDetails(index));
+            await Task.Run(() => ProcessCustomer());
+
+            Console.WriteLine("New Thread end at: {0}", DateTime.Now);
+
+        }
+        public static void GetCustomerDetails(int customerId)
+        {
+            Console.WriteLine("Database call long processsing ");
+            Thread.Sleep(5000);
+            Console.WriteLine("Customer Id: {0}", customerId);
+
+        }
+
+        public static void ProcessCustomer()
+        {
+            Thread.Sleep(2000);
+
+        }
+    }
+
+}
---
+++ threadingincsharp/asynawaitdemo/Step4/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asynawaitdemo.Step4
+{
+
+    class Employee
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Main thread Started");
+
+            GetAllEmployeeetails();
+            Console.WriteLine("Main thread do not wait for worker thread to be completed");
+
+            Console.ReadKey();
+
+        }
+
+        public static async void GetAllEmployeeetails()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+              await  RunMetodOnDifferentThread(i);
+            }
+
+        }
+
+        public static async Task  RunMetodOnDifferentThread(int index)
+        {
+            Console.WriteLine("Database call No : {0}", index);
+
+            Console.WriteLine("New Thread started at: {0}", DateTime.Now);
+
+            await Task.Run(new Action(GetEmployeeDetails));
+
+            Console.WriteLine("New Thread end at: {0}", DateTime.Now);
+
+        }
+        public static void GetEmployeeDetails()
+        {
+            Console.WriteLine("Long running method to get employee details and it will run on a single thread is processing.....");
+
+            Thread.Sleep(5000);
+            Console.WriteLine("Long running method is processing completed");
+
+
+        }
+    }
+}
